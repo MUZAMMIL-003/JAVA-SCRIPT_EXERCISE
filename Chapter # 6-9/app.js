2cc389015c18c424ac747a583b8a4341abc93047
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2024-09-26 12:09:23 -0700

    .

---
+++ Chapter # 6-9/app.js
@@ -0,0 +1,51 @@
+        // Question # 01
+var num = 15;
+console.log("the value of a is =" + num)
+console.log( num)
+
+console.log("..................")
+
+
+num = num
+console.log("The value of a++ is =",num)
+num = ++num
+console.log("Now the value of a++ is =",num)
+
+
+num = num++
+console.log("The value of ++a is =",num)
+console.log("Now the value of ++a is =",num)
+
+
+num = --num
+console.log("The value of --a is =",num)
+console.log("Now the value of --a is =",num)
+
+
+num = num
+console.log("The value of a-- is =",num)
+num = num--
+console.log("Now the value of a-- is =",num)
+
+    //    Question # 02
+
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+console.log(result);
+
+--a;
+console.log(a);
+--a - --b;
+console.log(result);
+--a - --b + ++b;
+console.log(result);
+--a - --b + ++b + b--;
+console.log(result);
+
+    //   Question # 03
+
+var userName = prompt("Enter Your Name")
+var greet = alert( "Hello" + "  " + userName  + "  "+ "Welcome!")
+
+    //    Question # 04
+       
