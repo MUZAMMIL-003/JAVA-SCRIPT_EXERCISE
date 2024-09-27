2af71f849b7c8a8c21ed6b465e6b05a19de44742
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2024-09-27 12:45:19 -0700

    solving some questions

---
+++ Chapter # 9-11/app.js
@@ -0,0 +1,64 @@
+// ______________Question # 01______________________
+// var cityName = prompt("Enter your city name");
+// var userCityName  = cityName.toLowerCase();
+// if(userCityName == "karachi"){
+//     alert("Hello, Karachi! May the city’s vibrant energy uplift you.")
+// }else if(userCityName == "lahore") {
+//     alert("Greetings from Lahore! May the culture and warmth of the city embrace you.")
+// }else if(userCityName == "islamabad") {
+//     alert("Welcome to the capital! May your visit be peaceful and inspiring.")
+// }else if(userCityName == "peshawar") {
+//     alert("Salam, Peshawar! May the rich history of this city inspire you.")
+// }else if(userCityName == "quetta") {
+//     alert( "Hello, Quetta! May the beauty of your landscapes bring you peace.")
+// }else if(userCityName == "faisalabad") {
+//     alert("Welcome to Faisalabad! Wishing you success in all your endeavors.")
+// }else if(userCityName == "multan") {
+//     alert( "Greetings from Multan! May your heart be filled with the city's rich heritage.")
+// }
+
+
+// ______________Question # 02______________________
+// var genders = prompt("Enter your gender");
+// var gender  = genders.toLowerCase();
+// if(gender == "male"){
+//     alert("Good Morning Sir.")
+// }else if(gender == "female") {
+//     alert("Good Morning Ma’am.")
+// }
+
+
+// ______________Question # 03______________________
+// var trafficColors = prompt("Enter any traffic signal color");
+// var colors  = trafficColors.toLowerCase();
+// if(colors == "red"){
+//     alert("Must Stop.")
+// }else if(gender == "yellow") {
+//     alert("Ready to move.")
+// }else if(gender == "green") {
+//     alert("Move now.")
+// }
+
+
+// ______________Question # 04______________________
+// var carFuel = prompt("Enter your car fuel in liters");
+// var fuel  = carFuel.toLowerCase();
+// if(fuel > 0.25 ){
+//     alert("Good to go")
+// }else  {
+//         alert("“Please refill the fuel in your car”.")
+// }
+
+// ______________Question # 05______________________
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else {
+//     alert("given condition for variable b is false")
+// }
